0da48c00612ae5a5f88bd1f1e5c8be11b7deb1b3
imajedalqahtani <imajed.alqahtani@gmail.com>
2026-05-05 02:19:36 +0300

    Update script.py

--- script.py
+++ script.py
@@ -5,47 +5,47 @@ from datetime import datetime
 def scan_ports(target, start_port, end_port):
 
     print(f"\nScanning target: {target}")  
-    print(f"Time started: {datetime.now()}\n")  # Show start time
+    print(f"Time started: {datetime.now()}\n")  # show start time
 
     try:
-        # Convert domain name into IP address
+        # convert domain name into IP address
         target_ip = socket.gethostbyname(target)
         print(f"IP Address: {target_ip}\n")
 
     except socket.gaierror:
-        # If domain name cannot be resolved
+        # if domain name cannot be resolved
         print("Hostname could not be resolved")
-        return  # Stop the function
+        return  # stop the function
 
-    open_ports = []  # List to store open ports
+    open_ports = []  # list to store open ports
 
-    # Loop through each port in the given range
+    # loop through each port in the given range
     for port in range(start_port, end_port + 1):
 
-        # Create a TCP socket (IPv4 + TCP)
+        # create a TCP socket (IPv4 + TCP)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Set timeout so we don't wait too long on each port
+        # set timeout so we don't wait too long on each port
         sock.settimeout(0.5)
 
-        # Try connecting to the port
+        # try connecting to the port
         result = sock.connect_ex((target_ip, port))
 
-        # If result is 0 then connection is successful then port is open
+        # if result is 0 then connection is successful then port is open
         if result == 0:
             print(f"[OPEN] Port {port}")
-            open_ports.append(port)  # Save open port
+            open_ports.append(port)  # save open port
 
         sock.close()  # close the connection
 
     print("\nScan complete.")  
-    print(f"Open ports: {open_ports}")  # Show results
+    print(f"Open ports: {open_ports}")  # show results
 
 
-# Ask user for input
+# ask user for input
 target = input("Enter target for example: (localhost or IP): ")
 start = int(input("Start port: "))
 end = int(input("End port: "))
 
-# Run the scanner
+# run the scanner
 scan_ports(target, start, end)
